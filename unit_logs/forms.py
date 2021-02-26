c87fa967a783fab5eb18ff18624a1ffe8a12b3cd
dimitrivanloey <dimitrivanloey@gmail.com>
2021-02-26 19:00:19 +0000

    reverting to previous

--- unit_logs/forms.py
+++ unit_logs/forms.py
@@ -26,7 +26,7 @@ class DenmanForm(forms.ModelForm):
 class KautoForm(forms.ModelForm):
     class Meta:
         model = Kauto
-        fields = ['number', 'status', 'entry_date']
+        fields = ['number', 'status']
 
 class FrankelForm(forms.ModelForm):
     class Meta:
